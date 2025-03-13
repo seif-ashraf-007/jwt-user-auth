6d84e341416d8acf51ab6eac3f2b60aa4562f866
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-14 00:13:02 +0200

    fixed an issue with the db env var naming

--- config/env.js
+++ config/env.js
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 config({ path: `./env/.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { NODE_ENV, PORT, MONGO_URI, JWT_SECRET, JWT_EXPIRES_IN } =
+export const { NODE_ENV, PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } =
   process.env;
