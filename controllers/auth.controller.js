e28b6d1b8beef5983d36bbcd91455b3c5a512000
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-14 01:22:19 +0200

    signup and signin controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,90 @@
-export const signUp = async (req, res) => {
-  res.send("POST signUp Controller");
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
+
+export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { name, email, role, password } = req.body;
+    const existingUser = await User.findOne({
+      email: email,
+    });
+
+    if (existingUser) {
+      throw new Error("User already exists", 409);
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashsedPassword = await bcrypt.hash(password, salt);
+
+    const newUsers = await User.create([
+      {
+        name,
+        email,
+        password: hashsedPassword,
+        role,
+      },
+    ]);
+
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        token,
+        user: newUsers[0],
+      },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
 };
 
-export const signIn = async (req, res) => {
-  res.send("POST signIn Controller");
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    const userPassword = user.password;
+
+    if (!user) {
+      throw new Error("Invalid credentials", 404);
+    }
+
+    if (!userPassword) {
+      throw new Error("Invalid credentials - pass", 404);
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, userPassword);
+
+    if (!isPasswordValid) {
+      throw new Error("Invalid credentials", 401);
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
