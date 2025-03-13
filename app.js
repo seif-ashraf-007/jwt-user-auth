dfab8bd5dafa8e1a4c938557e8a8d17270927b57
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-14 00:02:24 +0200

    Mount user and auth routes to their endpoints

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { PORT, NODE_ENV } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`[INFO] Server is running on port ${PORT} in ${NODE_ENV} mode`);
