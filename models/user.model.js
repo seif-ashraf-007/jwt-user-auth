60f9e476cbd1c7bfdeefa635f43b2f5566ef4ae6
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-14 00:18:11 +0200

    Create a schema/model to the User entity

---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxlength: [32, "Name cannot exceed 32 characters"],
+      minlength: [3, "Name must be at least 3 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
