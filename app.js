fba858aef83e872de551667b0b6cbbc4abfd33ca
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-14 00:24:32 +0200

    Used the error middleware

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ import { PORT, NODE_ENV } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`[INFO] Server is running on port ${PORT} in ${NODE_ENV} mode`);
