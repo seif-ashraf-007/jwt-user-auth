a4edd711843f122245ed896fd33500616f7df3bb
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-14 00:11:31 +0200

    Start DB connection when starting the application

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ import express from "express";
 import { PORT, NODE_ENV } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`[INFO] Server is running on port ${PORT} in ${NODE_ENV} mode`);
   console.log(`[INFO] http://localhost:${PORT}`);
+  await connectToDatabase();
 });
