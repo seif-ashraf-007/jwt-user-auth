e4a3c2b4397a369e427e791409e95f2c88c633cf
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-14 02:56:46 +0200

    controller to get all users from the database

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,18 @@
-export const getAllUsers = async (req, res) => {
-  res.send("GET User route");
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getUserById = async (req, res) => {
