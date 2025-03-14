5dc6544bd777f038e1baf33a6d28431132b7abd5
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-14 02:58:55 +0200

    Included the authorize and admin authorization middleware to protect the routes.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,12 +12,12 @@ const userRouter = Router();
 
 userRouter.get("/", authorize, authorizeAdmin("admin"), getAllUsers);
 
-userRouter.get("/:id", getUserById);
+userRouter.get("/:id", authorize, getUserById);
 
-userRouter.post("/", createUser);
+userRouter.post("/", authorize, authorizeAdmin("admin"), createUser);
 
-userRouter.put("/:id", updateUserById);
+userRouter.put("/:id", authorize, authorizeAdmin("admin"), updateUserById);
 
-userRouter.delete("/:id", deleteUserById);
+userRouter.delete("/:id", authorize, authorizeAdmin("admin"), deleteUserById);
 
 export default userRouter;
